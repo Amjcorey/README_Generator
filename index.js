bd1e9f90c7d2589786cd7ce1b9cf0bc996d177dc
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-04-21 21:19:34 -0700

    add code to create questions for user input, write readme file, and initialize app

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquier = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+        {
+          type: 'input',
+          name: 'title',
+          message: 'What is the project title?',
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Include a description',
+        },
+        {
+          type: 'input',
+          name: 'installation',
+          message: 'Include the installation instructions',
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: 'Include the usage information of this project',
+        },
+        {
+          type: 'input',
+          name: 'Contribution',
+          message: 'Include the contribution instructions',
+        },
+        //string
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please select a license:',
+            choices: ["Apache 2.0", "BSD 3", "GPL 3.0", "None"],
+          }, 
+          {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your GitHub username',
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address',
+          }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>{
+        err ? console.error(err) : console.log('Success!');
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquier.createPromptModule(question).then((response) => {
+        writeToFile("README.md", generateMarkdown(response));
+    });
+}
 
 // Function call to initialize app
 init();
